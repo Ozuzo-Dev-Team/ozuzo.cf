da6ee07ddf97e1b90a5fa2658cc5b118f7c18488
ChikuwaJB <yuki.mail.10.2@gmail.com>
2015-11-29 15:21:48 +0900

    Update

--- main.js
+++ main.js
@@ -1,4 +1,5 @@
 var i=0;
+
 function change(){
   i++;
   /*
@@ -14,13 +15,81 @@ function change(){
   $("strong").fadeIn(2000);
 }
 }
+var isshowmenu=0;
+function reg(){
+  $("#m > .fa-times").live("click",function(){
+if(isshowmenu==1){
+  isshowmenu=0;
+  $({  borderSpacing: 10 }).animate({  borderSpacing: 0 }, {
+      step: function(now,fx) {
+        $('#w').css({
+                  "-webkit-filter": "blur("+now+"px)",
+                  "filter": "blur("+now+"px)",
+                  "-ms-filter": "blur("+now+"px)"
+              });
+      },
+      complete: function(){
+        $("#m").fadeOut(500);
+        $("#m > .fa-times").die("click");
+      },
+
+      duration:'slow'
+  },'linear');
+}
+  });
+}
+$(function(){
+
+
+  $(".fa-bars").click(function(){
+    if(isshowmenu==0){
+
+    $({  borderSpacing: 0 }).animate({  borderSpacing: 10 }, {
+        step: function(now,fx) {
+          $('#w').css({
+                    "-webkit-filter": "blur("+now+"px)",
+                    "filter": "blur("+now+"px)",
+                    "-ms-filter": "blur("+now+"px)"
+                });
+        },
+        complete: function(){
+          $("#m").fadeIn(500);
+          reg();
+        },
+        duration:'slow'
+    },'linear');
+
+    isshowmenu=1;
+  }else{
+    isshowmenu=0;
+    $({  borderSpacing: 10 }).animate({  borderSpacing: 0 }, {
+        step: function(now,fx) {
+          $('#w').css({
+                    "-webkit-filter": "blur("+now+"px)",
+                    "filter": "blur("+now+"px)",
+                    "-ms-filter": "blur("+now+"px)"
+                });
+        },
+        complete: function(){
+          $("#m").fadeOut(500);
+        },
+
+        duration:'slow'
+    },'linear');
+
+
+  }
+});
+
+});
+
 $(document).ready(function(){
   var a=navigator.userAgent;
   if(-1!=a.search(/iPhone/)||-1!=a.search(/iPad/)||-1!=a.search(/Android/)){
   $("img").css("width","90%");
 }
 $("#w").fadeIn(2000);
-setInterval("change()",200);
+setInterval("change()",1000);
 });
 
 
